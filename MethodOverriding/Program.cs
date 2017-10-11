0b9a9dd017acdabbbfc35978acf0c64a56057162
Firefly4266 <gaines.johnp@gmail.com>
2017-10-10 22:22:28 -0700

    Completed exercize

--- MethodOverriding/Program.cs
+++ MethodOverriding/Program.cs
@@ -10,6 +10,13 @@ namespace MethodOverriding
     {
         static void Main(string[] args)
         {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            shapes.Add(new Triangle());
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
         }
     }
 }
---
+++ MethodOverriding/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Circle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine(" Draw a Circle\n");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine(" Draw a Rectangle\n");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine(" Draw a Triangle.\n");
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
